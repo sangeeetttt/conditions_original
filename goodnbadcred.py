4e454221e449455c8ea84bcd775008f1d46a3e6c
sangeeetttt <ssongeet500@gmail.com>
2021-06-18 12:12:26 +0545

    first commit

--- goodnbadcred.py
+++ goodnbadcred.py
@@ -3,9 +3,11 @@ Price of a house is $1M. If the buyer has good credit, they need to put down 10%
 Print the down payment.
 '''
 price = 1000000
-yourBank=int(input('The money that you have: '))
-good_credit = 2000000
-if good_credit == True:
-    print(10/100*price)
+yourBank = int(input('The money that you have: '))
+good_credit = False
+if good_credit:
+    down_payment = (10/100*price)
 else:
-    print(20/100*price)
+    down_payment = (20/100*price)
+
+print(f'downpayment is ${down_payment}')
--- weightConverter.py
+++ weightConverter.py
@@ -3,12 +3,14 @@ input weight of a person in either kg or lbs. if provides weight in kg, then con
 and viseversa.
 '''
 
-weight = int(input('the weight: '))
-unit = str(input('the unit of measurement you entered(kg or lbs): '))
+weight = int(input('Enter the weight: '))
+unit = input('(Kg) or (Lbs) : ').lower()
 
-if unit = kg:
-    inlbs= weight* 2.205
+if unit.lower() == "kg" :
+    inlbs = weight* 2.205
     print(inlbs)
-elif unit = lbs:
-    inkgs= weight/2.205
+elif unit.lower() == "lbs":
+    inkgs = weight/2.205
     print(inkgs)
+else:
+    print(f'invald')
